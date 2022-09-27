a181af98487176a6817d92ff7033cb36d3db1c3a
Mikhail Belov <bezprobeloff@gmail.com>
2022-09-27 10:47:04 +0300

    feat: проверка на несуществующий ресурс

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+const userRouter = require('./routes/user');
+const cardRouter = require('./routes/card');
 
 app.use((req, res, next) => {
   req.user = {
@@ -17,9 +19,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {});
 
-app.use('/users', require('./routes/user'));
-app.use('/cards', require('./routes/card'));
+app.use('/users', userRouter);
+app.use('/cards', cardRouter);
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
 });
